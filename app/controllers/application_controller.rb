82dfd0a2122aa7f5ed2301efea2c1ada65c27eaa
nikymorg <nicole.morgan@gmail.com>
2018-01-30 16:46:57 -0500

    add schedule functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,20 @@ class ApplicationController < Sinatra::Base
     erb :'schedule.html'
   end
 
+  post '/schedule/:id' do
+    @course = Course.find(params[:id])
+    courses = Course.where(on_schedule: true)
+    total_credits = courses.inject(0) {|sum, course| sum + course.credits}
+    if total_credits + @course.credits <= 15
+      @course.update(on_schedule: true)
+    end
+    redirect '/schedule'
+  end
+
+  delete '/schedule/:id' do
+    @course = Course.find(params[:id])
+    @course.update(on_schedule: false)
+    redirect '/schedule'
+  end
+
 end
